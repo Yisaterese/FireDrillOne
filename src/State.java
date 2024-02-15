4667feb7275d25e8bb22250dbc978c5a23d16a8c
Yisaterese <teresejosephyisa@gmail.com>
2024-02-15 21:35:50 +0100

    added code

--- src/State.java
+++ src/State.java
@@ -1,11 +1,18 @@
 public class State {
-    public GeoPoliticalZones getGeoPoliticalZones()(String inputState){
-        GeoPoliticalZones[] geoZones = GeoPoliticalZones.values();
-        for(int index = 0; index < geoZones.length; index++){
-            GeoPoliticalZones zonesStates = geoZones[index];
-            if(zonesStates.name().equalsIgnoreCase(inputState)) {
-                return zonesStates;
-            }
+    public GeoPoliticalZones getGeoPoliticalZones(String inputState) {
+        for(GeoPoliticalZones zones : GeoPoliticalZones.values()){
+            GeoPoliticalZones theZones = getStstes(inputState, zones);
+            if(theZones != null) return theZones;
+        }
+        return null;
+    }
+
+    private GeoPoliticalZones getStstes(String inputState, GeoPoliticalZones geoPoliticalZones){
+        String[] states = geoPoliticalZones.getStates();
+        for(int index = 0; index < states.length; index++){
+            if(states[index].equalsIgnoreCase(inputState))
+                return geoPoliticalZones;
+
         }
         return null;
     }
